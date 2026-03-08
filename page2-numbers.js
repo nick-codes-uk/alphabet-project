1f102f4eb297b69c537da86d851855d138b25ebf
nick-codes-uk <nick.amin99@outlook.com>
2026-03-08 13:08:26 +0000

    changes to the number bit which was wrong

--- page2-numbers.js
+++ page2-numbers.js
@@ -1,125 +1,34 @@
-
 let saveRandNum = 0;
 
-
-
-/* Return to page 1 if this button pressed   */
- 
+/* Return to page 1 */
 const numBtn = document.querySelector(".goto-page1");
+numBtn.addEventListener("click", goback);
 
-    numBtn.addEventListener("click", goback); 
+function goback() {
+  window.location.href = "index.html";
+}
 
-    function goback () {  
-    window.location.href = "index.html"; 
-    
-};
+/* Random number generator */
+const numCont = document.getElementById("numberContainer");
+const randBtn = document.querySelector(".random-num");
 
-/* get the random number and display it next to 
-  selection box.
-*/  
-  const numCont = document.getElementById("numberContainer");
-  const randBtn = document.querySelector(".random-num");
- 
+randBtn.addEventListener("click", getRandNum);
 
-  const randNumber = randBtn.addEventListener("click",getRandNum);              
-  
-function getRandNum () {   
-     
-     
-      const randNumber = Math.trunc(Math.random()*7)+1;   
-      
-     randNumber.html = `     
-      <div class="num-container">
-        <div class="the-num"">"${randNumber}"</div>
-       </div>      
-      `;
-     
-      document.querySelector(".the-num").innerHTML = randNumber;
-/*     
-      displayNumbers(randNumber);
-            
-   };
-   
+function getRandNum() {
+  // Clear previous numbers
+  numCont.innerHTML = "";
 
-   
+  // Generate random number 1–7
+  const randNumber = Math.trunc(Math.random() * 7) + 1;
 
-      function displayNumbers (randNumber) {
-*/      
-      for (let i = 1; i <= randNumber; i++) {        
-      const divItem = document.createElement("div");
+  // Display the number
+  document.querySelector(".the-num").innerText = randNumber;
 
-      divItem.classList.add("colour");     
-      divItem.innerText = i;
-      numCont.appendChild(divItem); 
-    
-
-      };   
-    
-   };
-
-function clrContainer() {   
-
-      let clrNumCont = document.getElementById("xyz"); 
-      
-       clrNumCont.innerText = "";
-
-    
- 
-};  
-
-
-
-
-
-
-
-
-
-
-/* working prog with function
-
-
- function getRandNum () {   
-      //       clrContainer ();     
-      const randNumber = Math.trunc(Math.random()*7)+1;   
-      
-     randNumber.html = `     
-      <div class="num-container">
-        <div class="the-num"">"${randNumber}"</div>
-       </div>      
-      `;
-     
-      document.querySelector(".the-num").innerHTML = randNumber;
-     
-      displayNumbers(randNumber);
-            
-   };
-   
-
-   
-
-      function displayNumbers (randNumber) {
-      
-      for (let i = 1; i <= randNumber; i++) {        
-      const divItem = document.createElement("div");
-
-      divItem.classList.add("colour");     
-      divItem.innerText = i;
-      numCont.appendChild(divItem); 
-    
-
-      };   
-    
-   };
-
-
-function clrContainer() {   
-
-      let clrNumCont = document.getElementById("xyz"); 
-      
-       clrNumCont.innerText = "";
-
-    
- 
-};  
-*/
+  // Create the coloured boxes
+  for (let i = 1; i <= randNumber; i++) {
+    const divItem = document.createElement("div");
+    divItem.classList.add("colour");
+    divItem.innerText = i;
+    numCont.appendChild(divItem);
+  }
+}
